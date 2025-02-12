13d0aa092a3f269df36c055374c013cee7af8e9e
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-12 15:10:00 +0700

    day_2

---
+++ day_1/main.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+name = input("What is your name? \n")
+pet = input("What is your pet's name? \n")
+print(f"Your band name could be {name} {pet}")
---
+++ day_2/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
+peoples = int(input("How many people to split the bill? "))
+pay = round((bill - tip) / peoples)
+print(f"Each person should pay: ${pay:.2f}")
